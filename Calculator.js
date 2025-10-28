7331ea77d19ea084328bfd031564ddb738f50477
Your Name <your_email@example.com>
2025-10-28 19:45:41 +0800

    Multiply

--- Calculator.js
+++ Calculator.js
@@ -1,10 +1,16 @@
 function add(a,b){
     return a+b
 }
+
 function subtract(a,b){
     return a-b
+}
+
+function multiply(a,b){
+    return a*b
 }
 let a = 5
 let b = 3
 console.log("ADD:", add(a, b));
 console.log("SUBTRACT:", subtract(a, b));
+console.log("MULTIPLY:", multiply(a, b));
