d0d5e7e19347c2b88a43c06c2417fc8eb88a1d8d
Your Name <your_email@example.com>
2025-10-28 19:47:53 +0800

    Divide

--- Calculator.js
+++ Calculator.js
@@ -9,8 +9,13 @@ function subtract(a,b){
 function multiply(a,b){
     return a*b
 }
+
+function divide(a,b){
+    return a/b
+}
 let a = 5
 let b = 3
 console.log("ADD:", add(a, b));
 console.log("SUBTRACT:", subtract(a, b));
 console.log("MULTIPLY:", multiply(a, b));
+console.log("DIVIDE:", divide(a, b));
